e5ab97f84d44b842e0d8117ec1346d757f2f90dd
AlexBHdez <abhbretones@gmail.com>
2018-02-12 16:58:59 +0100

    add logout local

--- cofluencer-app/routes/auth.js
+++ cofluencer-app/routes/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
 
 const Company = require('../models/company');
-// const Influencer = require('../models/influencer');
+const Influencer = require('../models/influencer');
 
 router.get('/signup', (req, res, next) => {
   res.render('auth/signup');
@@ -75,6 +75,16 @@ router.post('/login', (req, res, next) => {
         }
       });
   }
+});
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
--- cofluencer-app/routes/profile.js
+++ cofluencer-app/routes/profile.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const Company = require('../models/company');
+const authRedirect = require('../middlewares/auth');
+
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.render('profile');
+router.get('/', authRedirect('/login'), (req, res, next) => {
+  const userId = req.session.currentUser;
+
+  Company.findById(userId)
+    .then((company) => {
+      res.render('profile', { company });
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
