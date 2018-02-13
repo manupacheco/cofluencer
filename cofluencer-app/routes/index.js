e46425fe7183428848b6eb6e4bb168c6783b5424
Manu Pacheco <mpachecopal@gmail.com>
2018-02-13 20:41:42 +0100

    delete some pakage

--- cofluencer-app/routes/index.js
+++ cofluencer-app/routes/index.js
@@ -9,17 +9,15 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   const user = req.body;
-  console.log(user.name);
   FB.api(
     '/17841407080190618',
     {
       fields: `business_discovery.username( ${user.name} ){username,biography,website,followers_count,media_count,media{caption, comments_count,like_count, media_url, media_type}}`,
+      // incluir token en variable actual caduca 13 de abril de 2018.
       access_token: 'EAAZAp8OJ3y18BAAcfi7TMWZBVXkw8uHZAitgIQUWCvUI7hEbaNpeGTXLXyuZB9huheP84iiz1w8vY3ZC0tpxcoJOdWRilc9NPzNAo3zf9hpZC59fghyRRzG6c6KbzjFeYKVRD0GywHFhFQwYMMnZAO7PW5roFrcuTtLr0f0GihCh5QhghFdfPte',
     },
     (iguser) => {
       console.log(iguser);
-      console.log(iguser.business_discovery.username);
-      console.log('img -->', iguser.business_discovery.media.data[0].media_url);
       res.render('index', { titles: iguser.business_discovery.username, followers: iguser.business_discovery.followers_count, pics: iguser.business_discovery.media.data });
     },
   );
