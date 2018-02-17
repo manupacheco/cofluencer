b79547a50b3e3ca97cf4a2190455fa95fcd8f83a
Manu Pacheco <mpachecopal@gmail.com>
2018-02-17 12:45:24 +0100

    add function callInstagram

--- cofluencer-app/routes/profile.js
+++ cofluencer-app/routes/profile.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const configurePassport = require('../helpers/passport');
 /* eslint-disable */
 const isLoggedIn = require('../helpers/middlewares').isLoggedIn;
+const callInstagram = require('../helpers/middlewares').callInstagram;
 /* eslint-enable */
 const Company = require('../models/company');
 
@@ -21,6 +22,19 @@ router.get('/', isLoggedIn('/login'), (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+});
+
+router.post('/', (req, res, next) => {
+  const igUserName = req.body.name;
+  callInstagram(igUserName, (err, iguser) => {
+    if (err) {
+      console.log('err');
+      res.render('profile', {});
+    } else {
+      console.log(iguser);
+      res.render('profile', iguser);
+    }
+  });
 });
 
 module.exports = router;
